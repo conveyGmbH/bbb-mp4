e6da4174c254599ff29e81f4bf49c04da1f25594
Gilbert Schwaab <gilbert@convey.de>
2022-12-15 16:16:19 +0100

    Update bbb-mp4.sh

--- bbb-mp4.sh
+++ bbb-mp4.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 # Load .env variables
-set -a
-source <(cat  /var/www/bbb-mp4/config.env | \
-    sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
-set +a
 
-MEETING_ID=$1
-
-echo "converting $MEETING_ID to mp4" |  systemd-cat -p warning -t bbb-mp4
+echo "converting $1 to mp4" |  systemd-cat -p warning -t bbb-mp4
 
 sudo docker run --rm -d \
                 --name $MEETING_ID \
