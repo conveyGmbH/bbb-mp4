3f2cc165adf88f9f5f8d618d2c81dc708171bcc6
Gilbert Schwaab <gilbert@convey.de>
2022-12-16 16:40:32 +0100

    Update ffmpeg-cmd.sh

--- ffmpeg-cmd.sh
+++ ffmpeg-cmd.sh
@@ -6,9 +6,9 @@ DISPLAY_NUMBER=$2
 ffmpeg -y -nostats -draw_mouse 0 -s 1920x1080 \
 	-framerate 30 \
 # 	-b 2500k -minrate 300k -maxrate 3000k \
-	-f x11grab -thread_queue_size 4096 \
+	-f x11grab -thread_queue_size 1024 \
 	-i :$DISPLAY_NUMBER \
-	-f alsa -thread_queue_size 4096 \
+	-f alsa -thread_queue_size 102 \
 	-itsoffset 1.23 \
 	-i pulse -ac 2 \
 	-c:v libx264 -c:a aac  \
