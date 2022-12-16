d90323430349bcc5038077eb65de7317d8975650
Gilbert Schwaab <gilbert@convey.de>
2022-12-16 16:46:16 +0100

    Update ffmpeg-cmd.sh

--- ffmpeg-cmd.sh
+++ ffmpeg-cmd.sh
@@ -5,7 +5,6 @@ DISPLAY_NUMBER=$2
 #Record the BBB playback, playing in Google browser in xvfb virtual screen, as MP4 video
 ffmpeg -y -nostats -draw_mouse 0 -s 1920x1080 \
 	-framerate 30 \
-# 	-b 2500k -minrate 300k -maxrate 3000k \
 	-f x11grab -thread_queue_size 1024 \
 	-i :$DISPLAY_NUMBER \
 	-f alsa -thread_queue_size 1024 \
