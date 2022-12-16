ff9d1f7faa5d716494bd69b94f09796ae3bd0b80
Gilbert Schwaab <gilbert@convey.de>
2022-12-16 09:59:19 +0100

    Update bbb-mp4.sh

--- bbb-mp4.sh
+++ bbb-mp4.sh
@@ -4,8 +4,10 @@
 
 echo "converting $1 to mp4" |  systemd-cat -p warning -t bbb-mp4
 
+COPY_TO_LOCATION=/var/www/bigbluebutton-default/recording
+
 sudo docker run --rm -d \
-                --name $MEETING_ID \
+                --name $1 \
                 -v $COPY_TO_LOCATION:/usr/src/app/download \
                 --env MEETING_ID=$1 \
                 --env REC_URL=$2 \
