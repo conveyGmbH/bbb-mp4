cbb1c2abadab1de9b8794807a138c01db1bf8c69
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 23:20:51 +0100

    Update ffmpeg-out-1920-webm.sh

--- ffmpeg-out-1920-webm.sh
+++ ffmpeg-out-1920-webm.sh
@@ -1,7 +1,7 @@
 #!usr/bin/sh
 
 #Output as WEBM video
-echo "encoding $EXPORT_NAME-1920.webm started pass 1"
+echo "encoding $1-1920.webm started pass 1"
 ffmpeg -y \
        -i /usr/src/app/download/$1.mp4 \
        -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -an -f null /dev/null
