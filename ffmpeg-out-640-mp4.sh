9d6fc1ac9c8f0a1805b634f2328cae81bfb8db78
Gilbert Schwaab <gilbert@convey.de>
2022-12-20 00:54:49 +0100

    Update ffmpeg-out-640-mp4.sh

--- ffmpeg-out-640-mp4.sh
+++ ffmpeg-out-640-mp4.sh
@@ -4,7 +4,7 @@
 echo "encoding $1-640.mp4 started"
 ffmpeg -y \
 	-i /usr/src/app/download/$1.mp4 \
-	-c:v libx264 -minrate 500k -b:v 900K -vf 'scale=64:-1' -r 15 -preset slow -threads 0 \
+	-c:v libx264 -minrate 500k -b:v 900K -vf 'scale=640:-1' -r 15 -preset slow -threads 0 \
 	-movflags faststart \
 	/usr/src/app/download/$1-640.mp4
 echo "encoding $1-640.mp4 finished"
