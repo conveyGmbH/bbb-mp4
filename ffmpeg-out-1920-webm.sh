8788752938b470b5eec9627c7f5eaa086e5f3429
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 15:20:35 +0100

    Update ffmpeg-out-1920-webm.sh

--- ffmpeg-out-1920-webm.sh
+++ ffmpeg-out-1920-webm.sh
@@ -4,7 +4,7 @@ EXPORT_NAME=$1
 #Output as WEBM video
 ffmpeg -y \
        -i /usr/src/app/download/$EXPORT_NAME.mp4 \
-       -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -an -f null /dev/null && \
+       -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -vsync cfr -f null /dev/null
 ffmpeg -y \
        -i /usr/src/app/download/$EXPORT_NAME.mp4 \
        -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -c:a libopus \
