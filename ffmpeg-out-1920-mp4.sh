1fa99bd04e8f744a0c3571f05d789046b9aaf333
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 21:33:06 +0100

    Update ffmpeg-out-1920-mp4.sh

--- ffmpeg-out-1920-mp4.sh
+++ ffmpeg-out-1920-mp4.sh
@@ -6,7 +6,7 @@ echo "encoding $EXPORT_NAME-1920.mp4 started" >> /usr/src/app/download/bbb-mp4.l
 mkdir /usr/src/app/download/$EXPORT_NAME
 cd /usr/src/app/download/$EXPORT_NAME
 ffmpeg -y \
-	-i ..\$EXPORT_NAME.mp4 \
+	-i ../$EXPORT_NAME.mp4 \
 	-c:v libx264 \
 	-preset slow \
 	-crf 30 \
