d56935a4a875137707d02a79d486ab679bcaf4ec
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 21:19:12 +0100

    Update ffmpeg-out-1920-webm.sh

--- ffmpeg-out-1920-webm.sh
+++ ffmpeg-out-1920-webm.sh
@@ -1,15 +1,16 @@
 #!usr/bin/sh
 EXPORT_NAME=$1
 
-cd /usr/src/app/download
 #Output as WEBM video
 echo "encoding $EXPORT_NAME-1920.webm started pass 1" >> /usr/src/app/download/bbb-mp4.log
+mkdir /usr/src/app/download/$EXPORT_NAME
+cd /usr/src/app/download/$EXPORT_NAME
 ffmpeg -y \
-       -i /usr/src/app/download/$EXPORT_NAME.mp4 \
+       -i $EXPORT_NAME.mp4 \
        -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -an -f null /dev/null >> /usr/src/app/download/bbb-mp4.log
 echo "encoding $EXPORT_NAME-1920.webm started pass 2" >> /usr/src/app/download/bbb-mp4.log
 ffmpeg -y \
-       -i /usr/src/app/download/$EXPORT_NAME.mp4 \
+       -i $EXPORT_NAME.mp4 \
        -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -c:a libopus \
-       /usr/src/app/download/$EXPORT_NAME-1920.webm >> /usr/src/app/download/bbb-mp4.log
+       $EXPORT_NAME-1920.webm >> /usr/src/app/download/bbb-mp4.log
 echo "encoding $EXPORT_NAME-1920.webm finished" >> /usr/src/app/download/bbb-mp4.log
