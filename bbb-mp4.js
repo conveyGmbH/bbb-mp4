04b3c7d80932a2feb2a29c451af5be3438535906
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 22:08:53 +0100

    Update bbb-mp4.js

--- bbb-mp4.js
+++ bbb-mp4.js
@@ -141,8 +141,8 @@ async function main() {
         console.log("stopSync");
         xvfb.stopSync();
         if (ls) {
-            //console.log("kill sync ffmpeg job pid=" + ls.pid);
-            //kill(ls.pid);
+            console.log("kill sync ffmpeg job pid=" + ls.pid);
+            kill(ls.pid);
             console.log("now start transcoding");
             const ls_out_cmd = [
                 'ffmpeg-out-1920-mp4.sh', 
@@ -170,7 +170,7 @@ async function main() {
             }            
             for (var i = 0; i < ls_out_cmd.length; i++) {
                 console.log("call " + ls_out_cmd[i]);
-                child_process.spawn('sh', [
+                ls_out[i] = child_process.spawn('sh', [
                     ls_out_cmd[i], ' ',
                     `${exportname}`
                 ], {
