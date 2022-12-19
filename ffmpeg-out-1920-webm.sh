ea7aa954cf2dc4f0600db1cc14de6752763b2a69
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 22:39:25 +0100

    Update ffmpeg-out-1920-webm.sh

--- ffmpeg-out-1920-webm.sh
+++ ffmpeg-out-1920-webm.sh
@@ -2,13 +2,13 @@
 EXPORT_NAME=$1
 
 #Output as WEBM video
-echo "encoding $EXPORT_NAME-1920.webm started pass 1" >> /usr/src/app/download/bbb-mp4.log
+echo "encoding $EXPORT_NAME-1920.webm started pass 1"
 ffmpeg -y \
        -i /usr/src/app/download/$EXPORT_NAME.mp4 \
-       -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -an -f null /dev/null >> /usr/src/app/download/bbb-mp4.log
-echo "encoding $EXPORT_NAME-1920.webm started pass 2" >> /usr/src/app/download/bbb-mp4.log
+       -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 1 -an -f null /dev/null
+echo "encoding $EXPORT_NAME-1920.webm started pass 2"
 ffmpeg -y \
        -i /usr/src/app/download/$EXPORT_NAME.mp4 \
        -c:v libvpx-vp9 -b:v 0 -crf 30 -pass 2 -c:a libopus \
-       /usr/src/app/download/$EXPORT_NAME-1920.webm >> /usr/src/app/download/bbb-mp4.log
-echo "encoding $EXPORT_NAME-1920.webm finished" >> /usr/src/app/download/bbb-mp4.log
+       /usr/src/app/download/$EXPORT_NAME-1920.webm
+echo "encoding $EXPORT_NAME-1920.webm finished"
