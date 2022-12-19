48894311b1f56969940a5861a4b404f87a700c50
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 22:29:17 +0100

    Update ffmpeg-out-1280-webm.sh

--- ffmpeg-out-1280-webm.sh
+++ ffmpeg-out-1280-webm.sh
@@ -6,10 +6,10 @@ WIDTH=1280
 echo "encoding $EXPORT_NAME-$WIDTH.webm started pass 1" >> /usr/src/app/download/bbb-mp4.log
 ffmpeg -y \
        -i /usr/src/app/download/$EXPORT_NAME.mp4 \
-       -c:v libvpx -minrate 1M -b:v 1828K -vf 'scale=$WIDTH:-1' -r 24 -pass 1 -an -f null /dev/null >> /usr/src/app/download/bbb-mp4.log
+       -c:v libvpx-vp9 -minrate 1M -b:v 1828K -vf 'scale=$WIDTH:-1' -r 24 -pass 1 -an -f null /dev/null >> /usr/src/app/download/bbb-mp4.log
 echo "encoding $EXPORT_NAME-$WIDTH.webm started pass 2" >> /usr/src/app/download/bbb-mp4.log
 ffmpeg -y \
        -i /usr/src/app/download/$EXPORT_NAME.mp4 \
-       -c:v libvpx -minrate 1M -b:v 1828K -vf 'scale=$WIDTH:-1' -r 24 -pass 2 -c:a libopus \
+       -c:v libvpx-vp9 -minrate 1M -b:v 1828K -vf 'scale=$WIDTH:-1' -r 24 -pass 2 -c:a libopus \
        /usr/src/app/download/$EXPORT_NAME-$WIDTH.webm >> /usr/src/app/download/bbb-mp4.log
 echo "encoding $EXPORT_NAME-$WIDTH.webm finished" >> /usr/src/app/download/bbb-mp4.log
