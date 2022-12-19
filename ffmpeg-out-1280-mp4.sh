f1e3116782ab6c604de9f94ca1b791fef43539b1
Gilbert Schwaab <gilbert@convey.de>
2022-12-19 22:30:54 +0100

    Update ffmpeg-out-1280-mp4.sh

--- ffmpeg-out-1280-mp4.sh
+++ ffmpeg-out-1280-mp4.sh
@@ -6,7 +6,7 @@ WIDTH=1280
 echo "encoding $EXPORT_NAME-$WIDTH.mp4 started" >> /usr/src/app/download/bbb-mp4.log
 ffmpeg -y \
 	-i /usr/src/app/download/$EXPORT_NAME.mp4 \
-	-c:v libx264 -minrate 1M -b:v 1828K -vf 'scale=$WIDTH:-1' -r 24 \
+	-c:v libx264 -minrate 1M -b:v 1828K -vf 'scale=$WIDTH:-1' -r 24 -preset slow \
 	-movflags faststart \
 	/usr/src/app/download/$EXPORT_NAME-$WIDTH.mp4 >> /usr/src/app/download/bbb-mp4.log
 echo "encoding $EXPORT_NAME-$WIDTH.mp4 finished" >> /usr/src/app/download/bbb-mp4.log
