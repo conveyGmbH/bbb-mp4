b19db3b8b09a80710a486b969c9487d50dee6ec9
Gilbert Schwaab <gilbert@convey.de>
2022-12-22 15:07:34 +0100

    reverted to VP8

--- ffmpeg-out-1920-webm.sh
+++ ffmpeg-out-1920-webm.sh
@@ -5,11 +5,11 @@ echo "encoding $1-1920.webm started pass 1"
 mkdir /usr/src/app/download/$1
 ffmpeg -y \
        -i /usr/src/app/download/$1.mp4 \
-       -c:v libvpx-vp9 -crf 25 -pass 1 -an -f null /dev/null
+       -c:v libvpx -crf 25 -pass 1 -an -f null /dev/null
 echo "encoding $1-1920.webm started pass 2"
 ffmpeg -y \
        -i /usr/src/app/download/$1.mp4 \
-       -c:v libvpx-vp9 -crf 25 -pass 2 -c:a libopus \
-       /usr/src/app/download/$1/VP9-1920.webm
-echo "VP9-1920.webm" >> /usr/src/app/download/$1/files.txt
+       -c:v libvpx -crf 25 -pass 2 -c:a libopus \
+       /usr/src/app/download/$1/VP8-1920.webm
+echo "VP8-1920.webm" >> /usr/src/app/download/$1/files.txt
 echo "encoding $1-1920.webm finished"
